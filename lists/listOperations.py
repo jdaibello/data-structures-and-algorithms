b9296e2e2e48bc8b71f7782195adaaa0c381b77d
João Pedro <jpedrodaibello1999@outlook.com>
2024-03-21 11:31:40 -0300

    feat(lists): searching for an element an a list

--- lists/listOperations.py
+++ lists/listOperations.py
@@ -45,3 +45,24 @@ print(myList2)
 
 myList2.remove('c') # Remove element 'c'
 print(myList2)
+
+# Searching for an element in a list
+
+myList3 = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+
+# in operator
+target = 50
+
+if target in myList3:
+    print(f'{target} is in the list')
+else:
+    print(f'{target} is not in the list')
+
+# Linear search
+def linearSearch(list, target):
+    for i, value in enumerate(list):
+        if value == target:
+            return i
+    return -1
+
+print(linearSearch(myList3, target))
