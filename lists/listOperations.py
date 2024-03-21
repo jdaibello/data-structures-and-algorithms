eb3e7625035a9494438b027acd215263ea457611
João Pedro <jpedrodaibello1999@outlook.com>
2024-03-21 11:09:33 -0300

    feat(lists): update and insert

--- lists/listOperations.py
+++ lists/listOperations.py
@@ -11,3 +11,19 @@ for i in shoppingList:
 for i in range(len(shoppingList)):
     shoppingList[i] = shoppingList[i] + '+'
     print(shoppingList[i])
+
+# Update/Insert
+
+myList = [1, 2, 3, 4, 5, 6, 7]
+print(myList)
+
+myList[2] = 33 # Update
+myList[4] = 55 # Update
+print(myList)
+
+myList.insert(0, 11) # Insert 11 at index 0
+myList.append(55) # Add to the end
+
+newList = [11, 12, 13, 14]
+myList.extend(newList) # Add all elements of newList to myList
+print(myList)
