bcd68d1f01fdd543526b33108ba99c3448bb43da
João Pedro <jpedrodaibello1999@outlook.com>
2024-03-25 14:31:40 -0300

    feat(tuples): search for an element

--- tuples/tuple.py
+++ tuples/tuple.py
@@ -16,3 +16,20 @@ for i in newTuple:
 
 for i in range(len(newTuple)):
     print(newTuple[i])
+
+# How to search for an element in a tuple?
+
+print('a' in newTuple) # True
+print('f' in newTuple) # False
+
+print(newTuple.index('c')) # 2
+
+def searchTuple(tuple, element):
+    for i in range(0, len(tuple)):
+        if tuple[i] == element:
+            return print(f'The {element} is at index {i}')
+
+    return 'The element was not found'
+
+print(searchTuple(newTuple, 'c')) # The c is at index 2
+print(searchTuple(newTuple, 'f')) # The element was not found
