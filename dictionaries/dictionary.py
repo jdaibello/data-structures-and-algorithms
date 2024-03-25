4af001513883b286474185e4f988aabc1a35cc81
João Pedro <jpedrodaibello1999@outlook.com>
2024-03-25 10:10:18 -0300

    feat(dictionaries) dictionary methods

--- dictionaries/dictionary.py
+++ dictionaries/dictionary.py
@@ -49,3 +49,34 @@ print(myDict)
 removedElement = myDict.pop('address')
 print(removedElement)
 print(myDict)
+
+# Dictionary methods
+
+myDict = {'name': 'Jack', 'age': 26, 'address': 'London', 'education': 'Engineering'}
+
+# myDict.clear() # Removes all elements from dictionary
+print(myDict)
+
+myDict2 = myDict.copy() # Returns a shallow copy of dictionary
+print(myDict2)
+
+newDict = {}.fromkeys([1, 2, 3], 0) # Create a new dictionary with keys from sequence and values set to value
+print(newDict)
+
+print(myDict2.get('age', 27)) # Returns the value of the specified key. If key does not exist, returns default value
+
+print(myDict2.items()) # Returns a new view of the dictionary items as a Tuple (key, value)
+
+print(myDict2.keys()) # Returns keys of dictionary
+
+print(myDict2.popitem()) # Removes the last key-value pair from dictionary and returns it as Tuple
+print(myDict2)
+
+myDict2.setdefault('address', 'London') # Returns the value of the specified key. If key does not exist, inserts key with specified value
+print(myDict2)
+
+print(myDict2.values()) # Returns a list of dictionary values
+
+newDict2 = {'a': 1, 'b': 2, 'c': 3}
+myDict2.update(newDict2)
+print(myDict2) # Updates the dictionary with other dictionary
