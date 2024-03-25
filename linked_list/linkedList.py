ebb8ad99400b6aedc59e293671a8cd6728e374b2
João Pedro <jpedrodaibello1999@outlook.com>
2024-03-25 15:34:46 -0300

    feat(linked_list): create Singly Linked List append method

--- linked_list/linkedList.py
+++ linked_list/linkedList.py
@@ -4,11 +4,27 @@ class Node:
         self.next = None
 
 class LinkedList:
-    def __init__(self, value):
-        newNode = Node(value)
-        self.head = newNode
-        self.tail = newNode
-        self.length = 1
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
 
-newLinkedList = LinkedList(10)
+    def append(self, value):
+        newNode = Node(value)
+
+        if self.head is None:
+            self.head = newNode
+            self.tail = newNode
+        else:
+            self.tail.next = newNode
+            self.tail = newNode
+
+        self.length += 1
+
+        return self
+
+newLinkedList = LinkedList()
+newLinkedList.append(10)
+newLinkedList.append(20)
 print(newLinkedList.head.value)
+print(newLinkedList.tail.value)
