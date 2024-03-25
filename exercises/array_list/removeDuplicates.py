2b57226d15533cae9803bbb233bc25cdbc85fcf2
João Pedro <jpedrodaibello1999@outlook.com>
2024-03-25 08:46:41 -0300

    feat(exercises) add Remove Duplicates

--- exercises/array_list/removeDuplicates.py
+++ exercises/array_list/removeDuplicates.py
@@ -3,4 +3,15 @@ Remove the duplicate numbers on given integer array/list
 """
 
 def removeDuplicates(arr):
-    return list(set(arr))
+    uniqueList = []
+    seen = set()
+
+    for i in arr:
+        if i not in seen:
+            uniqueList.append(i)
+            seen.add(i)
+
+    return uniqueList
+
+myList = [1, 1, 2, 2, 3, 4, 5]
+print(removeDuplicates(myList)) # [1, 2, 3, 4, 5]
