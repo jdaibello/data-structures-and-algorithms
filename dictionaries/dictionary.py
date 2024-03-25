960d2f560b010c4dc0f9fc6686dddfe3d2df57dc
João Pedro <jpedrodaibello1999@outlook.com>
2024-03-25 09:16:43 -0300

    feat(dictionaries) create dictionary

---
+++ dictionaries/dictionary.py
@@ -0,0 +1,16 @@
+# Creating Dictionary
+
+myDict = dict()
+print(myDict)
+
+myDict2 = {}
+print(myDict2)
+
+engSp = dict(one='uno', two='dos', three='tres')
+print(engSp)
+
+engSp2 = {'one': 'uno', 'two': 'dos', 'three': 'tres'}
+print(engSp2)
+
+engSpList = [('one', 'uno'), ('two', 'dos'), ('three', 'tres')]
+print(dict(engSpList))
