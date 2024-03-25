e7bae4633d4f01a23fad5a5e8725a3e389aad567
João Pedro <jpedrodaibello1999@outlook.com>
2024-03-25 14:25:23 -0300

    feat(tuples) traversing a tuple

--- tuples/tuple.py
+++ tuples/tuple.py
@@ -8,3 +8,11 @@ print(newTuple2)
 print(newTuple[-2]) # d
 print(newTuple[1:3]) # ('b', 'c')
 print(newTuple[:]) # ('a', 'b', 'c', 'd', 'e')
+
+# How to traverse through a tuple?
+
+for i in newTuple:
+    print(i)
+
+for i in range(len(newTuple)):
+    print(newTuple[i])
