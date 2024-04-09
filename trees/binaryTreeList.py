0a473fbb724d4a89d9cd4a0223e1db30bf7a92f0
João Pedro <jpedrodaibello1999@outlook.com>
2024-04-09 16:05:23 -0300

    feat(tree): update Binary Tree using List

--- trees/binaryTreeList.py
+++ trees/binaryTreeList.py
@@ -7,6 +7,7 @@ class BinaryTree:
     def insertNode(self, value):
         if self.lastUsedIndex + 1 == self.maxSize:
             return "The Binary Tree is full"
+
         self.customList[self.lastUsedIndex+1] = value
         self.lastUsedIndex += 1
         return "The value has been successfully inserted"
@@ -15,11 +16,13 @@ class BinaryTree:
         for i in range(len(self.customList)):
             if self.customList[i] == nodeValue:
                 return "Success"
+
         return "Not found"
 
     def preOrderTraversal(self, index):
         if index > self.lastUsedIndex:
             return
+
         print(self.customList[index])
         self.preOrderTraversal(index*2)
         self.preOrderTraversal(index*2 + 1)
@@ -27,6 +30,7 @@ class BinaryTree:
     def inOrderTraversal(self, index):
         if index > self.lastUsedIndex:
             return
+
         self.inOrderTraversal(index*2)
         print(self.customList[index])
         self.inOrderTraversal(index*2+1)
@@ -34,6 +38,7 @@ class BinaryTree:
     def postOrderTraversal(self, index):
         if index > self.lastUsedIndex:
             return
+
         self.postOrderTraversal(index*2)
         self.postOrderTraversal(index*2+1)
         print(self.customList[index])
@@ -45,6 +50,7 @@ class BinaryTree:
     def deleteNode(self, value):
         if self.lastUsedIndex == 0:
             return "There is not any node to delete"
+
         for i in range(1, self.lastUsedIndex+1):
             if self.customList[i] == value:
                 self.customList[i] = self.customList[self.lastUsedIndex]
@@ -63,6 +69,9 @@ newBT.insertNode("Hot")
 newBT.insertNode("Cold")
 newBT.insertNode("Tea")
 newBT.insertNode("Coffee")
+
+print(newBT.searchNode("Tea"))
+print('****************')
 
 newBT.preOrderTraversal(1)
 print('****************')
