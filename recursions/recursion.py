3800dc1d7fbe93a36d55cdc365c6c3f05de041c3
João Pedro <jpedrodaibello1999@outlook.com>
2024-04-08 22:21:49 -0300

    feat(recursion): update Recursion

--- recursions/recursion.py
+++ recursions/recursion.py
@@ -62,7 +62,7 @@ print(powerOfTwoIt(4))
 
 def factorial(n):
     assert n >= 0 and int(n) == n, 'The number must be positive integer only!'
-    if n in [0,1]:
+    if n in [0, 1]:
         return 1
     else:
         return n * factorial(n - 1)
@@ -72,8 +72,8 @@ print(factorial(5))
 ## Fibonacci
 
 def fibonacci(n):
-    assert n >=0 and int(n) == n, 'Fibonacci number cannot be negative number or non integer.'
-    if n in [0,1]:
+    assert n >= 0 and int(n) == n, 'Fibonacci number cannot be negative number or non integer.'
+    if n in [0, 1]:
         return n
     else:
         return fibonacci(n - 1) + fibonacci(n - 2)
